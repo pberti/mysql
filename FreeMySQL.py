f571417460cec03b170e80537d1495f0280e60db
albert <albert@alberts-MacBook-Pro.local>
2022-04-18 11:41:07 +0300

    first

---
+++ FreeMySQL.py
@@ -0,0 +1,31 @@
+#SHIFT + Z TO EXECUTE
+
+#from turtle import clear
+from sqlalchemy import create_engine
+import pandas as pd
+#import matplotlib as plt
+#import seaborn as sns
+
+host="sql4.freemysqlhosting.net"
+database="sql4475967"
+user="sql4475967"
+password="wyqTTgFxLS"
+port = 3306
+
+def get_connection():
+    return create_engine(
+        url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
+            user, password, host, port, database
+        )
+    )
+
+
+sql = '''
+
+select * from guests;
+
+'''
+
+
+df = pd.read_sql(sql, get_connection())
+print (df)
